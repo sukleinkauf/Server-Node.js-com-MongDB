d070a348afdcc4518879b32ac65c167b9b4d7564
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-04-26 15:45:47 -0300

    adicionando funções gulp materialize

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,21 @@ var gulp = require('gulp'),
 	watch = require('gulp-watch'),
 	nodemon = require('gulp-nodemon');
 
-gulp.task('less',function(){
-	return gulp.src('less/*.less');
-	.pipe(less());
-	gulp.dest('public/stylesheets');
-	console.log("less");
+gulp.task('less', function () {//transpilação de less para css
+	return gulp.src('less/*.less')//origem
+	.pipe(less())//transpilação
+	.pipe(gulp.dest('public/stylesheets'));//destino
+	console.log("less")
 });
 
 gulp.task('materialize',function(){ //copiando da pasta de origim e colando no destino
 
 	gulp.src('bower_components/materialize/dist/css/materialize.min.css')//origem
-		.pipe(gulp.dest('public/stylesheets'));//destino
+		.pipe(gulp.dest('public/stylesheets'))//destino
 	gulp.src('bower_components/materialize/dist/js/materialize.min.js')
-		.pipe(gulp.dest('public/javascripts'));
+		.pipe(gulp.dest('public/javascripts'))
 	gulp.src('bower_components/materialize/dist/fonts/**/*')
-		.pipe(gulp.dest('public/fonts'));
+		.pipe(gulp.dest('public/fonts'))
 	console.log("materialize")
 });
 
