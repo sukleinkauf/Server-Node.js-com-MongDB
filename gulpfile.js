ec894d03ff3c0a542bdf127d9d6d297158b16ff7
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-04-26 15:41:57 -0300

    adicionando funções gulp

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+//requerições de funções gulps 
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	watch = require('gulp-watch'),
+	nodemon = require('gulp-nodemon');
+
+gulp.task('less',function(){
+	return gulp.src('less/*.less');
+	.pipe(less());
+	gulp.dest('public/stylesheets');
+	console.log("less");
+});
+
+gulp.task('materialize',function(){ //copiando da pasta de origim e colando no destino
+
+	gulp.src('bower_components/materialize/dist/css/materialize.min.css')//origem
+		.pipe(gulp.dest('public/stylesheets'));//destino
+	gulp.src('bower_components/materialize/dist/js/materialize.min.js')
+		.pipe(gulp.dest('public/javascripts'));
+	gulp.src('bower_components/materialize/dist/fonts/**/*')
+		.pipe(gulp.dest('public/fonts'));
+	console.log("materialize")
+});
+
+gulp.task('jquery',function(){ //copiando da pasta de origim e colando no destino
+	gulp.src('bower_components/jquery/dist/jquery.min.js')//origem
+		.pipe(gulp.dest('public/javascripts'));//destino
+	console.log("jquery")
+});
+
+gulp.task('nodemon', function () {//função nodemon que reenicia o servidor quando erros ou mudanças
+	nodemon({
+		script: './bin/www',
+		ext: 'js html'
+	})
+});
+
+gulp.task('default', ['nodemon','less','open'], function(){
+	gulp.watch("./less/**/*.less", ['less']);
+});
